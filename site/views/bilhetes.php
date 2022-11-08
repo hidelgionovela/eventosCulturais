1a803468eb63d7a466b144ebe7bb6bde494210f1
Hidélgio Novela <hidelgionovela@gmail.com>
2022-11-08 15:36:36 +0200

    Update bilhetes.php

--- site/views/bilhetes.php
+++ site/views/bilhetes.php
@@ -9,26 +9,40 @@ ob_start();
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/png" sizes="16x16" href="../img/download.jpg">
     <link rel="stylesheet" href="../assets/bootstrap.min.css">
     <link rel="stylesheet" href="../css/styleeventos.css">
+    <link rel="stylesheet" href="../css/menu.css">
     <title>Ver Bilhetes</title>
 </head>
 
 <body class="container">
     <?php include __DIR__ . '../../config/crud.php';
     $id = $_GET['id'];
-    
+
 
     $dados = read("Select * from bilhete INNER JOIN espectador ON espectador.espectador_id = bilhete.espectador_id where bilhete.evento_id = $id;");
 
     // var_dump($dados);
     ?>
+    <div class="header__nav">
+        <img src="../img/download.jpg" style="width: 60px; border-radius:5%; float: left; margin-left:10px; margin-top:5px;" alt="logo">
+        <h2 style="float: left">Lista de Eventos</h2>
+        <nav class="header__menu mobile-menu">
+            <ul>
+                <li class="active"><a href="dashbord.php">Home</a></li>
+                <li><a href="cadastroUser.php">Criar User</a></li>
+                <li><a href="cadastro.php">Criar Evento</a></li>
+            </ul>
+        </nav>
+    </div>
     <div class="divCentral">
         <center>
             <h1>Lista venda de Bilhetes</h1>
             <h3>Nome do evento: <?php $dado = read("Select nome from evento where codigo = $id");
-             foreach ($dado as $a) {  
-            echo $a['nome'];}
+                                foreach ($dado as $a) {
+                                    echo $a['nome'];
+                                }
                                 ?></h3>
 
             <?php $h = read("SELECT COUNT(*) FROM bilhete where bilhete.evento_id = $id;");
@@ -36,7 +50,7 @@ ob_start();
                 echo "<h2>" . "Total de Vendas:  " . "#" . $k['COUNT(*)'] . "</h2>";
             }
             ?>
-            <img src="../img/download.jpg" style="width: 80px; border-radius:5%;" alt="logo">
+            <!-- <img src="../img/download.jpg" style="width: 80px; border-radius:5%;" alt="logo"> -->
             <hr>
         </center>
         <br><br>
